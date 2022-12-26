0042b889962bbe3adb44158696ea87b0a27869b7
Kevin Arias <kariasduarte@gmail.com>
2022-12-26 17:50:22 -0500

    agregue botones

--- src/componentes/container/sectionContacto/SectionContacto.jsx
+++ src/componentes/container/sectionContacto/SectionContacto.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import './sectionContacto.css';
+import whatsapp from './img/whatsapp.png';
+import gmail from './img/gmail.png';
+import nequi from './img/nequi.png';
+import daviplata from './img/daviplata.png';
 
 export function SectionContacto () {
     return(
@@ -12,7 +16,25 @@ export function SectionContacto () {
                     <input className="section2__container--problema" type="text" placeholder="Ingresa el tipo de problema de tu carro"/> 
                 </div>
                 <div className="section2__container--btns">
-                    <button>Envialo por whatsapp</button>
+                    <button className="section2__container--bton">
+                        <img src={whatsapp} alt="" />
+                        Envíalo por whatsapp
+                    </button>
+                    <button className="section2__container--bton">
+                        <img src={gmail} alt="" />
+                        Envíalo por gmail
+                    </button>
+                </div>
+                <h2 className="aside__container--h2">Puntos de pago</h2>
+                <div className="section2__container--btns">
+                    <button className="section2__container--pays">
+                        <img src={nequi} alt="" />
+                        Paga con nequi
+                    </button>
+                    <button className="section2__container--pays">
+                        <img src={daviplata} alt="" />
+                        Envíalo por daviplata
+                    </button>
                 </div>
             </div>
         </section>
