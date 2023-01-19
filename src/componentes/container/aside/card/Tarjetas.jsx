6a5ab6c7400d5eb151b42d6f4cdc0c8614cdb839
Kevin Arias <kariasduarte@gmail.com>
2023-01-18 21:23:59 -0500

    Arreglo de imagenes de presentación

--- src/componentes/container/aside/card/Tarjetas.jsx
+++ src/componentes/container/aside/card/Tarjetas.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Card, CardImg} from 'react-bootstrap';
+import './tarjetas.css';
 
 const Tarjetas = (props) => {
     let {imgSrc} = props.data;
